0f27135ef274387dc8646b4076161d8256074de3
qzy03715 <139351243+qzy03715@users.noreply.github.com>
2024-01-23 17:31:29 +0800

    把命令行常用指令放在这儿了

--- fastchat/llm_judge/1.py
+++ fastchat/llm_judge/1.py
@@ -6,11 +6,14 @@ model_dir = snapshot_download('qwen/Qwen-1_8B-Chat')
 
 '''
 下面是命令行指令
+
 获取模型答案
 python gen_model_answer.py --model-path /home/featurize/Qwen-1_8B-Chat --model-id Qwen-1_8B-Chat
+
 生成打分
 python gen_judgment.py --model-list Qwen-1_8B-Chat
-汇总打分
+
+将打分进行汇总
 python show_result.py
 
 '''
